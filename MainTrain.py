69b8e5d8e2e5739461c0778e920a04c54e01c56f
Franfee <599450863@qq.com>
2023-06-28 21:19:41 +0800

    adjust lr_rate for optimizer_unet

--- MainTrain.py
+++ MainTrain.py
@@ -230,7 +230,7 @@ def Stage3_Train_Com():
 
     # -----------------------------------
     optimizer_vae = torch.optim.AdamW(vae1.parameters(), lr=1e-5, betas=(0.5, 0.999), weight_decay=0.01, eps=1e-8)
-    optimizer_unet = torch.optim.AdamW(unet1.parameters(), lr=1e-4, betas=(0.8, 0.999), weight_decay=0.01, eps=1e-8)
+    optimizer_unet = torch.optim.AdamW(unet1.parameters(), lr=1e-5, betas=(0.5, 0.999), weight_decay=0.01, eps=1e-8)
     criterion_l1 = torch.nn.L1Loss()
     criterion_l2 = torch.nn.MSELoss()
 
